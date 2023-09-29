99a87abddd0d431e1bcf8ecbabdcfc6228ed69c9
Nikita Romanov <romanovnnikita@gmail.com>
2023-09-29 22:53:36 +0300

    add modal and adaptation

--- src/js/script.js
+++ src/js/script.js
@@ -82,4 +82,20 @@ document.querySelector(".next").addEventListener("click", function () {
       $(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
     });
   });
-})(jQuery);
+});
+
+//Скрипт на модальные окна (появление, закрытие)
+$("[data-modal=consultation]").on("click", function () {
+  $(".overlay, #consultation").fadeIn("slow");
+});
+
+$(".modal__close").on("click", function () {
+  $(".overlay, #consultation, #order, #thanks").fadeOut("slow");
+});
+
+$(".button_mini").each(function (i) {
+  $(this).on("click", function () {
+    $("#order .modal__descr").text($(".catalog-item__subtitle").eq(i).text());
+    $(".overlay, #order").fadeIn("slow");
+  });
+});
