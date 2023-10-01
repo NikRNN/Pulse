e8ef08f78c3c4c9ac9ac39c6fcd3f6e15967a64b
Nikita Romanov <romanovnnikita@gmail.com>
2023-10-01 14:42:52 +0300

    Work with openserver

--- src/js/script.js
+++ src/js/script.js
@@ -44,25 +44,25 @@ document.querySelector(".next").addEventListener("click", function () {
 });
 
 // код для переключения между табами(фитнес, бег, триатлон)
-(function ($) {
-  $(function () {
-    $("ul.catalog__tabs").on(
-      "click",
-      "li:not(.catalog__tab_active)",
-      function () {
-        $(this)
-          .addClass("catalog__tab_active")
-          .siblings()
-          .removeClass("catalog__tab_active")
-          .closest("div.container")
-          .find("div.catalog__content")
-          .removeClass("catalog__content_active")
-          .eq($(this).index())
-          .addClass("catalog__content_active");
-      }
-    );
-  });
 
+$(document).ready(function () {
+  $("ul.catalog__tabs").on(
+    "click",
+    "li:not(.catalog__tab_active)",
+    function () {
+      $(this)
+        .addClass("catalog__tab_active")
+        .siblings()
+        .removeClass("catalog__tab_active")
+        .closest("div.container")
+        .find("div.catalog__content")
+        .removeClass("catalog__content_active")
+        .eq($(this).index())
+        .addClass("catalog__content_active");
+    }
+  );
+
+  //перелистывание окон внутри табов (окно с ценами меняется на описание при нажатии на подробнее)
   $(".catalog-item__link").each(function (i) {
     $(this).on("click", function (e) {
       e.preventDefault();
@@ -73,6 +73,7 @@ document.querySelector(".next").addEventListener("click", function () {
     });
   });
 
+  //перелистывание окон обратно при нажатии на кнопку назад
   $(".catalog-item__back").each(function (i) {
     $(this).on("click", function (e) {
       e.preventDefault();
@@ -82,20 +83,85 @@ document.querySelector(".next").addEventListener("click", function () {
       $(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
     });
   });
-});
 
-//Скрипт на модальные окна (появление, закрытие)
-$("[data-modal=consultation]").on("click", function () {
-  $(".overlay, #consultation").fadeIn("slow");
-});
+  // // вариант функции для двух верхних блоков кода
+  // function toggleslide(item) {
+  //   $(item).each(function (i) {
+  //     $(this).on("click", function (e) {
+  //       e.preventDefault();
+  //       $(".catalog-item__content")
+  //         .eq(i)
+  //         .toggleClass("catalog-item__content_active");
+  //       $(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
+  //     });
+  //   });
+  // }
 
-$(".modal__close").on("click", function () {
-  $(".overlay, #consultation, #order, #thanks").fadeOut("slow");
-});
+  // toggleslide(".catalog-item__link");
+  // toggleslide(".catalog-item__back");
 
-$(".button_mini").each(function (i) {
-  $(this).on("click", function () {
-    $("#order .modal__descr").text($(".catalog-item__subtitle").eq(i).text());
-    $(".overlay, #order").fadeIn("slow");
+  //Скрипт на модальные окна (появление, закрытие)
+  $("[data-modal=consultation]").on("click", function () {
+    $(".overlay, #consultation").fadeIn("slow");
+  });
+
+  $(".modal__close").on("click", function () {
+    $(".overlay, #consultation, #order, #thanks").fadeOut("slow");
+  });
+
+  $(".button_mini").each(function (i) {
+    $(this).on("click", function () {
+      $("#order .modal__descr").text($(".catalog-item__subtitle").eq(i).text());
+      $(".overlay, #order").fadeIn("slow");
+    });
+  });
+
+  //работа с валидацией форм с помощью плагина от jQuery validate
+
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: "required",
+        phone: "required",
+        email: {
+          required: true,
+          email: true,
+        },
+      },
+      messages: {
+        name: "Пожалуйста, введите своё имя",
+        phone: "Пожалуйста, введите свой телефон",
+        email: {
+          required: "Пожалуйста, введите свой почтовый адрес",
+          email: "Неправильно введен почтовый адрес",
+        },
+      },
+    });
+  }
+
+  validateForms("#consultation-form");
+  validateForms("#order form");
+  validateForms("#consultation form");
+
+  //маска для ввода номера
+  $("input[name=phone]").mask("+7 (999) 999-99-99 ");
+
+  //отправка писем с сайта на сервер
+  $("form").submit(function (e) {
+    e.preventDefault();
+    // if (!$(this).valid()) {
+    //   return;
+    // }
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize(),
+    }).done(function () {
+      $(this).find("input").val("");
+      $("#consultation, #order").fadeOut();
+      $(".overlay, #thanks").fadeIn();
+      $("form").trigger("reset");
+    });
+    return false;
   });
 });
