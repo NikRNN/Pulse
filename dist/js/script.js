c6e8bc82b7913e0b3fa04d5a2d8a31e8d4752fb6
Nikita Romanov <romanovnnikita@gmail.com>
2023-10-05 22:18:49 +0300

    last commit

---
+++ dist/js/script.js
@@ -0,0 +1,185 @@
+// $(document).ready(function () {
+//   $(".carousel__inner").slick({
+//     speed: 1000,
+//     // adaptiveHeight: true,
+//     prevArrow:
+//       '<button type="button" class="slick-prev"><img src="../icons/left.png"></button>',
+//     nextArrow:
+//       '<button type="button" class="slick-next"> <img src= "../icons/right.png"> </button>',
+//     responsive: [
+//       {
+//         breakpoint: 992,
+//         settings: {
+//           arrows: false,
+//           dots: true,
+//         },
+//       },
+//     ],
+//   });
+// });
+
+// код для работы слайдера + адаптация
+const slider = tns({
+  container: ".carousel__inner",
+  items: 1,
+  slideBy: "page",
+  autoplay: false,
+  controls: false,
+  navPosition: "bottom",
+  nav: true,
+  responsive: {
+    768: {
+      nav: false,
+    },
+  },
+});
+
+// код для переключения стрелок на слайдере
+document.querySelector(".prev").addEventListener("click", function () {
+  slider.goTo("prev");
+});
+
+document.querySelector(".next").addEventListener("click", function () {
+  slider.goTo("next");
+});
+
+// код для переключения между табами(фитнес, бег, триатлон)
+
+$(document).ready(function () {
+  $("ul.catalog__tabs").on(
+    "click",
+    "li:not(.catalog__tab_active)",
+    function () {
+      $(this)
+        .addClass("catalog__tab_active")
+        .siblings()
+        .removeClass("catalog__tab_active")
+        .closest("div.container")
+        .find("div.catalog__content")
+        .removeClass("catalog__content_active")
+        .eq($(this).index())
+        .addClass("catalog__content_active");
+    }
+  );
+
+  //перелистывание окон внутри табов (окно с ценами меняется на описание при нажатии на подробнее)
+  $(".catalog-item__link").each(function (i) {
+    $(this).on("click", function (e) {
+      e.preventDefault();
+      $(".catalog-item__content")
+        .eq(i)
+        .toggleClass("catalog-item__content_active");
+      $(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
+    });
+  });
+
+  //перелистывание окон обратно при нажатии на кнопку назад
+  $(".catalog-item__back").each(function (i) {
+    $(this).on("click", function (e) {
+      e.preventDefault();
+      $(".catalog-item__content")
+        .eq(i)
+        .toggleClass("catalog-item__content_active");
+      $(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
+    });
+  });
+
+  // // вариант функции для двух верхних блоков кода
+  // function toggleslide(item) {
+  //   $(item).each(function (i) {
+  //     $(this).on("click", function (e) {
+  //       e.preventDefault();
+  //       $(".catalog-item__content")
+  //         .eq(i)
+  //         .toggleClass("catalog-item__content_active");
+  //       $(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
+  //     });
+  //   });
+  // }
+
+  // toggleslide(".catalog-item__link");
+  // toggleslide(".catalog-item__back");
+
+  //Скрипт на модальные окна (появление, закрытие)
+  $("[data-modal=consultation]").on("click", function () {
+    $(".overlay, #consultation").fadeIn("slow");
+  });
+
+  $(".modal__close").on("click", function () {
+    $(".overlay, #consultation, #order, #thanks").fadeOut("slow");
+  });
+
+  $(".button_mini").each(function (i) {
+    $(this).on("click", function () {
+      $("#order .modal__descr").text($(".catalog-item__subtitle").eq(i).text());
+      $(".overlay, #order").fadeIn("slow");
+    });
+  });
+
+  //работа с валидацией форм с помощью плагина от jQuery validate
+
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: "required",
+        phone: "required",
+        email: {
+          required: true,
+          email: true,
+        },
+      },
+      messages: {
+        name: "Пожалуйста, введите своё имя",
+        phone: "Пожалуйста, введите свой телефон",
+        email: {
+          required: "Пожалуйста, введите свой почтовый адрес",
+          email: "Неправильно введен почтовый адрес",
+        },
+      },
+    });
+  }
+
+  validateForms("#consultation-form");
+  validateForms("#order form");
+  validateForms("#consultation form");
+
+  //маска для ввода номера
+  $("input[name=phone]").mask("+7 (999) 999-99-99 ");
+
+  //отправка писем с сайта на сервер
+  $("form").submit(function (e) {
+    e.preventDefault();
+    // if (!$(this).valid()) {
+    //   return;
+    // }
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize(),
+    }).done(function () {
+      $(this).find("input").val("");
+      $("#consultation, #order").fadeOut();
+      $(".overlay, #thanks").fadeIn();
+      $("form").trigger("reset");
+    });
+    return false;
+  });
+
+  //настройка скролла и элемента вверх
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1600) {
+      $(".pageup").fadeIn();
+    } else {
+      $(".pageup").fadeOut();
+    }
+  });
+
+  //скрипт для плавной прокрутки (в хроме, лисичке по умолчанию плавный скролл работает)
+  $("a[href^='#']").click(function () {
+    const _href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+    return false;
+  });
+
+  new WOW().init();
+});
--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,19 @@ const sass = require("gulp-sass")(require("sass"));
 const rename = require("gulp-rename");
 const autoprefixer = require("gulp-autoprefixer");
 const cleanCSS = require("gulp-clean-css");
+const imagemin = require("gulp-imagemin");
+const htmlmin = require("gulp-htmlmin");
 
-// Static server
+// чтобы browsersync работал и обновлялся
 gulp.task("server", function () {
   browserSync.init({
     server: {
-      baseDir: "src",
+      baseDir: "dist",
     },
   });
 });
+
+gulp.watch("src/*.html").on("change", browserSync.reload);
 
 gulp.task("styles", function () {
   return gulp
@@ -31,13 +35,53 @@ gulp.task("styles", function () {
     )
     .pipe(cleanCSS({ compatibility: "ie8" }))
 
-    .pipe(gulp.dest("src/css"))
+    .pipe(gulp.dest("dist/css"))
     .pipe(browserSync.stream());
 });
 
 gulp.task("watch", function () {
-  gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
-  gulp.watch("src/*.html").on("change", browserSync.reload);
+  gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel("styles"));
+  gulp.watch("src/*.html").on("change", gulp.parallel("html"));
 });
 
-gulp.task("default", gulp.parallel("watch", "server", "styles"));
+gulp.task("html", function () {
+  gulp
+    .src("src/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("dist/"));
+});
+
+gulp.task("scripts", function () {
+  gulp.src("src/js/**/*.js").pipe(gulp.dest("dist/js"));
+});
+
+gulp.task("fonts", function () {
+  gulp.src("src/fonts/**/*").pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task("icons", function () {
+  gulp.src("src/icons/**/*").pipe(gulp.dest("dist/icons"));
+});
+
+gulp.task("mailer", function () {
+  gulp.src("src/mailer/**/*").pipe(gulp.dest("dist/mailer"));
+});
+
+gulp.task("images", function () {
+  gulp.src("src/img/**/*").pipe(imagemin()).pipe(gulp.dest("dist/img"));
+});
+
+gulp.task(
+  "default",
+  gulp.parallel(
+    "watch",
+    "server",
+    "styles",
+    "scripts",
+    "fonts",
+    "icons",
+    "mailer",
+    "images",
+    "html"
+  )
+);
